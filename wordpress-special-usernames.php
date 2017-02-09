c67711c28fc355bdb60ccb9246571615ad5d9a1a
uğur mirza zeyrek <github@mersinuni.net>
2017-02-09 15:14:41 +0200

    altered with simpler method allow special username

--- wordpress-special-usernames.php
+++ wordpress-special-usernames.php
@@ -17,22 +17,20 @@ if(!defined('ABSPATH')) exit;
  * @wp-hook wpmu_validate_user_signup
  * @param   array $result
  * @return  array
- * @source http://stackoverflow.com/questions/32047894/username-only-lowercase-and-numbers-in-wp-multi/32054345#32054345
  */
-function wpmu_no_username_error( $result ) {
-    $error_name = $result[ 'errors' ]->get_error_messages( 'user_name' );
-    if ( empty ( $error_name ) 
-        or false===$key=array_search( __( 'Usernames can only contain lowercase letters (a-z) and numbers.' ), $error_name)
-    ) {
-    return $result;
-    }
-//  only remove the error we are disabling, leaving all others
-    unset ( $result[ 'errors' ]->errors[ 'user_name' ][$key] );
-/**
- *  re-sequence errors in case a non sequential array matters
- *  e.g. if a core change put this message in element 0 then get_error_message() would not behave as expected)
- */
-    $result[ 'errors' ]->errors[ 'user_name' ] = array_values( $result[ 'errors' ]->errors[ 'user_name' ] );
-    return $result;
+function allow_special_usernames( $result ) {
+$error_name = $result[ 'errors' ]->get_error_message( 'user_name' );
+
+if ( ! empty ( $error_name ) 
+      && $error_name == __( 'Usernames can only contain lowercase letters (a-z) and numbers.' ) 
+       && $result['user_name'] == $result['orig_username'] 
+       && ! preg_match( '/[^-a-z0-9]/', $result['user_name'] ) 
+   ) {
+     unset ( $result[ 'errors' ]->errors[ 'user_name' ] );
+     return $result;
+   } else {
+     return $result;
+   }
 }
-add_filter( 'wpmu_validate_user_signup', 'wpmu_no_username_error' );
+
+add_filter( 'wpmu_validate_user_signup', 'allow_special_usernames' );
