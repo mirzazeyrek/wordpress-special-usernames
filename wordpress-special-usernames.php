60035b0b273a245d76ce77c2ebbb08bdfc4cb35d
uğur mirza zeyrek <github@mersinuni.net>
2017-02-14 07:34:45 +0200

    remove minimum character limit.

--- wordpress-special-usernames.php
+++ wordpress-special-usernames.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WordPress Special Usernames
 Plugin URI: https://github.com/mirzazeyrek/wordpress-special-usernames
-Description: Allow special characters in usernames.
+Description: Allow special characters and remove minimum character limit in usernames.
 Tags: username, multi-site, special characters, restrictions
 Version: 1.0
 Author: Ugur Mirza Zeyrek
@@ -12,7 +12,7 @@ Author URI: http://mirzazeyrek.wordpress.com
 if(!defined('ABSPATH')) exit;
 
 /**
- * Allow special usernames
+ * Allow special usernames and remove minimum character limit.
  *
  * @wp-hook wpmu_validate_user_signup
  * @param   array $result
@@ -22,7 +22,8 @@ function allow_special_usernames( $result ) {
 $error_name = $result[ 'errors' ]->get_error_message( 'user_name' );
 
 if ( ! empty ( $error_name ) 
-      && $error_name == __( 'Usernames can only contain lowercase letters (a-z) and numbers.' ) 
+      && ( $error_name == __( 'Usernames can only contain lowercase letters (a-z) and numbers.' ) 
+ 	|| $error_name == __( 'Username must be at least 4 characters.' ) )
        && $result['user_name'] == $result['orig_username'] 
        && ! preg_match( '/[^-a-z0-9]/', $result['user_name'] ) 
    ) {
